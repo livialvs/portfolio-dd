62bb3e63f9a9fe35ce9d8c764f94c0de6bc7eee6
Livia Alves de Faria <126177124+livialvs@users.noreply.github.com>
2023-05-16 18:07:36 -0300

    Adicionei novo app.py sem erros

--- src/app.py
+++ src/app.py
@@ -3,25 +3,26 @@ from jinja2 import Template
 
 app = Flask("__name__")
 
-@app.route("/")
+
+@app.route('/')
 def home():
     return render_template("home.html")
 
-@app.route("/home")
-def home2():
-    return render_template("home.html")
 
-@app.route("/academico")
+@app.route('/academico')
 def academico():
     return render_template("academico.html")
 
-@app.route("/sobre")
+
+@app.route('/sobre')
 def sobre():
     return render_template("sobre.html")
 
-@app.route("/contatos")
+
+@app.route('/contatos')
 def contatos():
     return render_template("contatos.html")
 
+
 if __name__ == '__main__':
- app.run()
+    app.run()
